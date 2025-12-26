b02bee034e150f1129780f4302e561afafded334
Alok Thakur <aloksingh.thakur@kilowott.com>
2025-12-26 12:57:34 +0530

    Update CORS for production

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -25,7 +25,13 @@ const app: Application = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+     origin: [
+       'http://localhost:5173',
+       'https://t-journey.vercel.app/', // Your Vercel URL
+     ],
+     credentials: true,
+   }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
